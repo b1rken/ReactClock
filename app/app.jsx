3edf903a01d5f2f3587d56117e42b12421435608
Espen Birk <espenbirk@Espens-MacBook-Pro.local>
2017-05-09 21:49:42 +0200

    Support for Foundation settings scss

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,6 @@ var Countdown = require('Countdown');
 
 
 // Load Foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App CSS
--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -6,6 +6,7 @@ var CountdownForm = React.createClass({
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
 
+    console.log('input count', $('input').length);
     if (strSeconds.match(/^[0-9]*$/) && strSeconds !== '') {
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
